649e76e5db8f76921ef66fab355ecf1547de7a23
SirLa <siranushik94@gmail.com>
2017-05-28 17:58:22 +0400

    menu update

--- src/Menu.js
+++ src/Menu.js
@@ -1,21 +1,15 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import './StyleSheet/Menu.css';
-
-class Menu extends Component{
-  render(){
+const Menu=({...props})=> {
     return(
-      
-          <div className="menu">
-                  <ul className="list_menu">
-                      <li className="menu_item"><NavLink activeClassName="active" to='/table'>Table </NavLink></li>
-                      {/*<li className="menu_item"><NavLink activeClassName="active" to='/addmailinglist'>MailList </NavLink></li>
-                      <li className="menu_item"><NavLink activeClassName="active" to='/sendemails'> Send Email </NavLink></li>*/}
-                 </ul>
-          </div>
-      
-    );
-  }
+      <div className={props.openStatus ? "open menu" : "menu" }>
+        <ul className="list_menu">
+          <li className="menu_item"><NavLink activeClassName="active" to='/table'>Table </NavLink></li>
+          {/*<li className="menu_item"><NavLink activeClassName="active" to='/addmailinglist'>MailList </NavLink></li>
+          <li className="menu_item"><NavLink activeClassName="active" to='/sendemails'> Send Email </NavLink></li>*/}
+        </ul>
+      </div>
+      );
 }
-
 export default Menu;
